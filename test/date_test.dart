a625b9471e0183a7642c5c9e0e058cae0235c545
Aleksey Kulikov <skinny.mind@gmail.com>
2024-05-30 16:34:07 +0300

    feat(date): add more API methods (#14)

--- test/date_test.dart
+++ test/date_test.dart
@@ -1,5 +1,6 @@
 import 'package:imitatio/imitatio.dart';
 import 'package:imitatio/src/datasets/date.dart';
+import 'package:imitatio/src/datasets/international/date.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -32,6 +33,102 @@ void main() {
         final data = DateData.locale[locale.name]!.day['abbr']!;
         expect(data.contains(result), true);
       }
+    });
+
+    test('returns a year', () {
+      final currentYear = DateTime.now().year;
+      final defaultResult = Date.year();
+      expect((defaultResult >= 1990) && (defaultResult <= currentYear), true);
+
+      final min = 1800;
+      final max = 2000;
+      final minMaxResult = Date.year(min: min, max: max);
+      expect((minMaxResult >= min) && (minMaxResult <= max), true);
+    });
+
+    test('returns a week number with year', () {
+      final currentYear = DateTime.now().year;
+      final defaultResult = Date.weekDate();
+      final splitDefault =
+          defaultResult.replaceFirst('-', ' ').replaceFirst('W', '').split(' ');
+      final (year, week) =
+          (int.parse(splitDefault[0]), int.parse(splitDefault[1]));
+      expect((year >= 2017) && (year <= currentYear), true);
+      expect((week >= 1) && (week <= 52), true);
+
+      final start = 1800;
+      final end = 2000;
+      final minMaxResult = Date.weekDate(start: start, end: end);
+      final splitMinMax =
+          minMaxResult.replaceFirst('-', ' ').replaceFirst('W', '').split(' ');
+      final (minMaxYear, minMaxWeek) =
+          (int.parse(splitMinMax[0]), int.parse(splitMinMax[1]));
+      expect((minMaxYear >= start) && (minMaxYear <= end), true);
+      expect((minMaxWeek >= 1) && (minMaxWeek <= 52), true);
+    });
+
+    test('returns name of the month', () {
+      final result = Date.month();
+      final data = DateData.locale[Locale.en.name]!.month['name']!;
+      expect(data.contains(result), true);
+
+      for (final locale in Locale.values) {
+        final result = Date.month(locale: locale);
+        final data = DateData.locale[locale.name]!.month['name']!;
+        expect(data.contains(result), true);
+      }
+    });
+
+    test('returns abbreviated name of the month', () {
+      final result = Date.month(isAbbr: true);
+      final data = DateData.locale[Locale.en.name]!.month['abbr']!;
+      expect(data.contains(result), true);
+
+      for (final locale in Locale.values) {
+        final result = Date.month(isAbbr: true, locale: locale);
+        final data = DateData.locale[locale.name]!.month['abbr']!;
+        expect(data.contains(result), true);
+      }
+    });
+
+    test('returns century', () {
+      final result = Date.century();
+      expect(IntDateData.romanNums.contains(result), true);
+    });
+
+    test('returns periodicity', () {
+      final result = Date.periodicity();
+      final data = DateData.locale[Locale.en.name]!.periodicity;
+      expect(data.contains(result), true);
+
+      for (final locale in Locale.values) {
+        final result = Date.periodicity(locale: locale);
+        final data = DateData.locale[locale.name]!.periodicity;
+        expect(data.contains(result), true);
+      }
+    });
+
+    test('returns day of month', () {
+      final result = Date.dayOfMonth();
+      expect((result >= 1) && (result <= 31), true);
+    });
+
+    test('returns random timezone', () {
+      final result = Date.timezone();
+      final data = IntDateData.timezones.values.expand((e) => e);
+      expect(data.contains(result), true);
+    });
+
+    test('returns random timezone for provided region', () {
+      final region = TimezoneRegion.europe;
+      final result = Date.timezone(region: region);
+      final data = IntDateData.timezones[region.name]!;
+      expect(data.contains(result), true);
+    });
+
+    test('returns GMT offset value', () {
+      final result = Date.gmtOffset();
+      expect(IntDateData.gmtOffsets.contains(result), true);
     });
   });
 }
