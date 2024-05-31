ab4996df32cf36f8aa88adc7519d7ef3b0c8de4c
Aleksey Kulikov <skinny.mind@gmail.com>
2024-05-31 16:08:24 +0300

    refactor(date): use getters instead of variables

--- lib/src/datasets/en/date.dart
+++ lib/src/datasets/en/date.dart
@@ -4,74 +4,82 @@ class EnDateData extends DateDataset {
   EnDateData();
 
   @override
-  final day = const <String, List<String>>{
-    "abbr": [
-      "Fri.",
-      "Mon.",
-      "Sat",
-      "Sun.",
-      "Thu.",
-      "Tue.",
-      "Wed.",
-    ],
-    "name": [
-      "Friday",
-      "Monday",
-      "Saturday",
-      "Sunday",
-      "Thursday",
-      "Tuesday",
-      "Wednesday",
-    ],
-  };
+  Map<String, List<String>> get day {
+    return const {
+      "abbr": [
+        "Fri.",
+        "Mon.",
+        "Sat",
+        "Sun.",
+        "Thu.",
+        "Tue.",
+        "Wed.",
+      ],
+      "name": [
+        "Friday",
+        "Monday",
+        "Saturday",
+        "Sunday",
+        "Thursday",
+        "Tuesday",
+        "Wednesday",
+      ],
+    };
+  }
 
   @override
-  final formats = const <String, String>{
-    "date": "%m/%d/%Y",
-    "time": "%H:%M:%S",
-  };
+  Map<String, String> get formats {
+    return const {
+      "date": "%m/%d/%Y",
+      "time": "%H:%M:%S",
+    };
+  }
 
   @override
-  final month = const <String, List<String>>{
-    "abbr": [
-      "Jan.",
-      "Feb.",
-      "Mar.",
-      "Apr.",
-      "May.",
-      "Jun.",
-      "Jul.",
-      "Aug.",
-      "Sept.",
-      "Oct.",
-      "Nov.",
-      "Dec.",
-    ],
-    "name": [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ],
-  };
+  Map<String, List<String>> get month {
+    return const {
+      "abbr": [
+        "Jan.",
+        "Feb.",
+        "Mar.",
+        "Apr.",
+        "May.",
+        "Jun.",
+        "Jul.",
+        "Aug.",
+        "Sept.",
+        "Oct.",
+        "Nov.",
+        "Dec.",
+      ],
+      "name": [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+    };
+  }
 
   @override
-  final periodicity = const <String>[
-    "Daily",
-    "Monthly",
-    "Never",
-    "Often",
-    "Once",
-    "Seldom",
-    "Weekly",
-    "Yearly",
-  ];
+  List<String> get periodicity {
+    return const [
+      "Daily",
+      "Monthly",
+      "Never",
+      "Often",
+      "Once",
+      "Seldom",
+      "Weekly",
+      "Yearly",
+    ];
+  }
 }
