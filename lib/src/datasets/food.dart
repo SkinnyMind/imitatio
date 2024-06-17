036c8122fbc5fc63205a7a93a4b53da26f8d5b96
Aleksey Kulikov <skinny.mind@gmail.com>
2024-06-17 19:39:00 +0300

    feat(food): add API methods

---
+++ lib/src/datasets/food.dart
@@ -0,0 +1,16 @@
+import 'package:imitatio/src/datasets/en/food.dart';
+import 'package:imitatio/src/datasets/models/food.dart';
+import 'package:imitatio/src/datasets/ru/food.dart';
+import 'package:imitatio/src/enums.dart';
+
+/// Provides all data related to the food.
+class FoodData {
+  const FoodData._();
+
+  static FoodDataset locale(Locale locale) {
+    return switch (locale) {
+      Locale.en => EnFoodData(),
+      Locale.ru => RuFoodData(),
+    };
+  }
+}
---
+++ lib/src/datasets/models/food.dart
@@ -0,0 +1,7 @@
+abstract class FoodDataset {
+  List<String> get dishes;
+  List<String> get drinks;
+  List<String> get fruits;
+  List<String> get spices;
+  List<String> get vegetables;
+}
