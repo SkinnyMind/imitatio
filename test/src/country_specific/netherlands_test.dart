ff70eb16de83ec61a7d882b761ea767e8e057812
Aleksey Kulikov <skinny.mind@gmail.com>
2024-07-03 15:57:25 +0300

    feat(country_specific): add Netherlands specific API method

---
+++ test/src/country_specific/netherlands_test.dart
@@ -0,0 +1,28 @@
+import 'package:imitatio/imitatio.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Netherlands', () {
+    final netherlands = CountrySpecific.netherlands();
+    final seededNetherlands = CountrySpecific.netherlands(seed: 1);
+
+    test('returns BSN number', () async {
+      final result = netherlands.bsn;
+      expect(result.length, equals(9));
+
+      final test11 = int.parse(result[0]) * 9 +
+          int.parse(result[1]) * 8 +
+          int.parse(result[2]) * 7 +
+          int.parse(result[3]) * 6 +
+          int.parse(result[4]) * 5 +
+          int.parse(result[5]) * 4 +
+          int.parse(result[6]) * 3 +
+          int.parse(result[7]) * 2 +
+          int.parse(result[8]) * -1;
+
+      expect(test11 % 11, equals(0));
+
+      expect(seededNetherlands.bsn, equals(seededNetherlands.bsn));
+    });
+  });
+}
