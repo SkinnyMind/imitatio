8edb87a23d250bb8a22c38009efba03eca10b66f
Aleksey Kulikov <skinny.mind@gmail.com>
2026-01-09 15:08:09 +0300

    feat(cryptographic): add `hash` API method

--- lib/src/cryptographic.dart
+++ lib/src/cryptographic.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+import 'package:imitatio/src/enums.dart';
 import 'package:imitatio/src/util.dart';
 
 /// Provides pseudo-cryptographic data.
@@ -17,4 +21,24 @@ class Cryptographic {
   /// Cryptographic().uuid; // "a462502a-73af-4e41-bfc4-05957b7030dd"
   /// ```
   String get uuid => Util.uuidV4(seed: seed);
+
+  /// Returns random hash.
+  ///
+  /// [algorithm] is one of [Algorithm].
+  ///
+  /// Example:
+  /// ```dart
+  /// Cryptographic().hash(algorithm: Algorithm.md5); // "74d85e475b3d23ce0618f5b6763a1143"
+  /// ```
+  String hash({required Algorithm algorithm}) {
+    final bytes = utf8.encode(uuid);
+    return switch (algorithm) {
+      .md5 => md5.convert(bytes).toString(),
+      .sha1 => sha1.convert(bytes).toString(),
+      .sha224 => sha224.convert(bytes).toString(),
+      .sha256 => sha256.convert(bytes).toString(),
+      .sha384 => sha384.convert(bytes).toString(),
+      .sha512 => sha512.convert(bytes).toString(),
+    };
+  }
 }
